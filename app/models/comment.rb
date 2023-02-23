cced7650c01f8df6dc24072c01e3f4b11e56e6be
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-23 22:17:16 +0200

    Add the model for the entity of comment

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
+
+  def update_comment_counter
+    post.increment(:comments_counter)
+  end
+end
