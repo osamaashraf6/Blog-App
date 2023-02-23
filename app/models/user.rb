2df03613c3ac9263ab270f72a7c69baa5c4f1a4b
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-23 22:18:13 +0200

    Add the model for the entity of user

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+end
