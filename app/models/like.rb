74f250cd0c2dd259bd40864a851a8d4b4d508972
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-23 22:17:55 +0200

    Add the model for the entity of like

---
+++ app/models/like.rb
@@ -0,0 +1,8 @@
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
+
+  def update_like_counter
+    post.increment(:likes_counter)
+  end
+end
