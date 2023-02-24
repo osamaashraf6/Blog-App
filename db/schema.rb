61674f7f1d480acca727386351837addb977d055
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-24 12:07:46 +0200

    Try to fix the linter error

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# rubocop:disable Metrics/BlockLength
 ActiveRecord::Schema[7.0].define(version: 20_230_222_155_905) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -61,3 +62,4 @@ ActiveRecord::Schema[7.0].define(version: 20_230_222_155_905) do
   add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
+# rubocop:enable Metrics/BlockLength
