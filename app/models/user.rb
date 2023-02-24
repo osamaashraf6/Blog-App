b6e73031c7f88de7269815ead234e73987b3ac4b
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-24 17:39:04 +0200

    Add the test for the user model and Add the validation for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+  
+  validates :name, presence: true
+  validates :postsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
