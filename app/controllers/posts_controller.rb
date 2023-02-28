32357b08515fd013666700d16ab5ec1c6cbe3199
osamaashraf6 <osamaashraf668@gmail.com>
2023-02-28 14:51:18 +0200

    Add the controller for the posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+  end
+end
