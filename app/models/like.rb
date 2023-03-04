8419b4f377bfb46639921713a2058e3896dac915
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-04 10:23:27 +0200

    Fix the linter error

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
-  
   def update_like_counter
     post.increment!(:likesCounter)
   end
