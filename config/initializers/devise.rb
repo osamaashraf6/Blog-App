b7e1b06a72d00a58b71fe0c7d5e68f0c19a3564e
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-09 21:49:56 +0200

    Fix the linter error

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Layout/LineLength
+
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
@@ -309,3 +311,5 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 end
+
+# rubocop:enable Layout/LineLength
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+# rubocop:enable Layout/LineLength
+
 ActiveRecord::Schema[7.0].define(version: 20_230_309_152_328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -70,3 +72,5 @@ ActiveRecord::Schema[7.0].define(version: 20_230_309_152_328) do
   add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
+
+# rubocop:enable Layout/LineLength
