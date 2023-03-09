79956d230846742ade832c2079b9e09313ea335c
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-09 21:53:02 +0200

    Fix the linter error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# rubocop:enable Layout/LineLength
+# rubocop:disable Metrics/BlockLength
+
 
 ActiveRecord::Schema[7.0].define(version: 20_230_309_152_328) do
   # These are extensions that must be enabled in order to support this database
@@ -72,5 +73,3 @@ ActiveRecord::Schema[7.0].define(version: 20_230_309_152_328) do
   add_foreign_key 'likes', 'users', column: 'author_id'
   add_foreign_key 'posts', 'users', column: 'author_id'
 end
-
-# rubocop:enable Layout/LineLength
