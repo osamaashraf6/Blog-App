4fd680ab6cc3eb2a75b8b5b9c2917a5608a728b4
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-10 15:23:14 +0200

    Modify the model user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
