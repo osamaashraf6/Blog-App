0863891bb3138f5cef7d7527fef5758ba0324b62
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-10 15:21:48 +0200

    Modify the model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :commentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
