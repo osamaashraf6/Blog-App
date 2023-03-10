3bc7f8af05139298e7af07d593c9ecebd1085f66
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-10 15:22:43 +0200

    Modify the routes file for the deleting

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,8 @@ Rails.application.routes.draw do
   #   resources :posts
   # end
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
-      resources :comments, only: %i[new create]
+    resources :posts, only: %i[index show new create destroy] do
+      resources :comments, only: %i[new create destroy]
       resources :likes, only: %i[new create]
     end
   end
