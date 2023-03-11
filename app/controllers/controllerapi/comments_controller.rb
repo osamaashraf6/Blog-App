e56bc741cb8445cbd3bda05c5049c0e94c51dbad
momotsow <kgomotsomkansi@gmail.com>
2023-03-11 13:22:07 +0200

    Fix comments controller

---
+++ app/controllers/controllerapi/comments_controller.rb
@@ -0,0 +1,30 @@
+class Api::CommentsController < ApplicationController
+    before_action :authorize_request
+    protect_from_forgery with: :null_session
+  
+    def index
+      @comments = Comment.where(post_id: params[:post_id])
+  
+      if @comments.empty?
+        json_response({ msg: 'Not found' }, 400)
+      else
+        render json: @comments
+      end
+    end
+  
+    def create
+      @comment = Comment.create(
+        text: comment_params[:text],
+        user_id: params[:user_id],
+        post_id: params[:post_id]
+      )
+  
+      json_response(@comment, :created)
+    end
+  
+    private
+  
+    def comment_params
+      params.require(:comment).permit(:text)
+    end
+  end
