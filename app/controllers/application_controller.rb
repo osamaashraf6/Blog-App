e7f038511808e10367749fd8aa85bfe16d0595dd
osamaashraf6 <osamaashraf668@gmail.com>
2023-03-11 13:15:33 +0200

    Add the API for the blog

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
-# class ApplicationController < ActionController::Base
-#   # def current_user
-#   #   User.first
-#   # end
-# end
 class ApplicationController < ActionController::Base
+  include Response
+  include ExceptionHandler
+
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
@@ -13,5 +11,19 @@ class ApplicationController < ActionController::Base
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+end
+
+def authorize_request
+  header = request.headers['Auth']
+  header = header.split.last if header
+  begin
+    @decoded = JsonWebToken.decode(header)
+    p @decoded
+    @current_user = User.find(@decoded['id'])
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { errors: e.message }, status: :unauthorized
+  rescue JWT::DecodeError => e
+    render json: { error: e.message }, status: :unauthorized
   end
 end
